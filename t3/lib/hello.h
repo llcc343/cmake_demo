d39052a6753356aa48a8e73a7667e3460026acf6
longchao343 <382078448@qq.com>
2017-06-04 20:32:23 +0800

    exsersise using cmake

---
+++ t3/lib/hello.h
@@ -0,0 +1,10 @@
+#ifndef HELLO_H
+#define HEELO_H
+
+#include <iostream>
+
+using namespace std;
+
+void HelloFunc();
+
+#endif
---
+++ t4/build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/x/workspace/cmake_examples/t4/src/main.cpp" "/home/x/workspace/cmake_examples/t4/build/src/CMakeFiles/main.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/x/workspace/cmake_examples/t3/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
